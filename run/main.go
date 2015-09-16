f03f08f8b6a71d7d34f7eb5836bf083919588e83
kiwih <hammondpearce@outlook.com>
2015-09-16 21:12:22 +1200

    Refine program arguments being passed in by environment variables. Cookiestoresalt no longer constant.

--- run/main.go
+++ run/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"flag"
+	"io"
+	"log"
+	"os"
 
 	"github.com/kiwih/heyfyi"
 )
 
-var (
-	logFileName   = flag.String("logfilename", "heyfyi.txt", "Specify the name of the log file")
-	serverAddress = flag.String("serveraddress", ":3000", "Specify the address the server should listen on")
-)
-
 func main() {
-	heyfyi.StartServer(*logFileName, *serverAddress)
+	//Enable logger
+	logFileName := os.Getenv("LOG_FILE_NAME")
+	if len(logFileName) == 0 {
+		logFileName = "heyfyi.txt"
+	}
+
+	f, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic("Can't open log file: " + err.Error())
+	}
+	log.SetOutput(io.MultiWriter(f, os.Stdout))
+
+	// On platforms like heroky/dokku this should be PORT, not HTTP_PORT. On Azure this should be HTTP_PLATFORM_PORT
+	serverAddress := ":" + os.Getenv("HTTP_PORT")
+	if serverAddress == ":" {
+		log.Println("$HTTP_PORT was not set, defaulting to 3000")
+		serverAddress = ":3000"
+	}
+
+	cookieStoreSalt := os.Getenv("COOKIE_STORE_SALT")
+	if len(cookieStoreSalt) == 0 {
+		log.Fatal("$COOKIE_STORE_SALT was not set, cannot go on. Exiting.")
+	}
+
+	heyfyi.StartServer(serverAddress, cookieStoreSalt)
 }
--- server.go
+++ server.go
@@ -4,10 +4,8 @@ import (
 	"encoding/gob"
 	"encoding/json"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"reflect"
 	"strconv"
 	"time"
@@ -20,39 +18,13 @@ import (
 )
 
 var (
-	COOKIE_STORE_SALT string		/* Cryptographic salt for cookie signing/encrypting */
-	HTTP_PORT string						/* TCP port for web server */
+	store *sessions.CookieStore
 
-	store sessions.CookieStore
-
-	templates = template.Must(template.New("").Funcs(funcMap).ParseGlob("./templates/*"))                 //this initializes the template engine
-	decoder   = schema.NewDecoder()                                                                       //this initializes the schema (HTML form decoding) engine
+	templates = template.Must(template.New("").Funcs(funcMap).ParseGlob("./templates/*")) //this initializes the template engine
+	decoder   = schema.NewDecoder()                                                       //this initializes the schema (HTML form decoding) engine
 )
 
-func StartServer(logFileName string, serverAddress string) {
-
-	f, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		panic("Can't open log file: " + err.Error())
-	}
-	log.SetOutput(io.MultiWriter(f, os.Stdout))
-
-
-	/* On platforms like heroky/dokku this should be PORT, not HTTP_PORT. */
-	HTTP_PORT = os.Getenv("HTTP_PORT")
-	if len(HTTP_PORT) == 0 {
-		log.Println("$HTTP_PORT was not set, defaulting to 3000")
-		HTTP_PORT = "3000"
-	}
-
-	COOKIE_STORE_SALT = os.Getenv("COOKIE_STORE_SALT")
-	if len(COOKIE_STORE_SALT) == 0 {
-		log.Fatal("$COOKIE_STORE_SALT was not set, cannot go on. Exiting.")
-	} else {
-		store = sessions.NewCookieStore([]byte("9s7YD807h*&DHhihSD123434SASDD__834HUSJNCxczc123!@#sd85"))
-	}
-
-
+func StartServer(serverAddress string, cookieStoreSalt string) {
 
 	//gob is used when we save failed form structs to the session
 	gob.Register(CreateAccount{})
@@ -62,12 +34,14 @@ func StartServer(logFileName string, serverAddress string) {
 
 	fyidb.ConnectDatabase("heyfyi")
 
+	store = sessions.NewCookieStore([]byte(cookieStoreSalt))
+
 	router := initRouter()
 
 	go BackgroundVoteGiver()
 
 	log.Println("Server running at " + serverAddress)
-	if err := http.ListenAndServe(":"+HTTP_PORT, router); err != nil {
+	if err := http.ListenAndServe(serverAddress, router); err != nil {
 		log.Println("Error:", err.Error())
 	}
 }
